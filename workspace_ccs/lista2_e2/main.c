99d922d0eb07535b72416acc102cce1025e1d420
leonamcsg <leonamcsg@hotmail.com>
2022-07-27 03:16:02 -0300

    Aplicando debouncing simples

--- workspace_ccs/lista2_e2/main.c
+++ workspace_ccs/lista2_e2/main.c
@@ -1,9 +1,9 @@
-#include <msp430.h> 
-
+#include <msp430.h>
+#include <stdint.h>
 
 /**
  * Botao P2.1 (pressionado P2.1 = 0) altera estado do Led P1.0 (ativo-alto)
- * Ao pressionar S1 o Led1 altera estado. Sem "debouncing".
+ * Ao pressionar S1 o Led1 altera estado. Com "debouncing".
  */
 
 int IOconfig(void) {
@@ -16,14 +16,25 @@ int IOconfig(void) {
     return 0;
 }
 
+int debounce(void) {
+    volatile uint16_t delay_loops = 10000; // MCLOCK em 1MHz, delay ~= 10ms
+    while(--delay_loops);
+
+    return 0;
+}
+
 int main(void) {
     WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
 
     IOconfig();
 
     for(;;) {
-        if((P2IN & BIT1) == 0)
+        if((P2IN & BIT1) == 0) {
             P1OUT ^= (BIT0);    // altera estado do led
+            debounce();
+            while((P2IN & BIT1) == 0); // Caso botao nao tenha sido solto
+            debounce();                // prende a execucao
+        }
     }
     return 0;
 }
