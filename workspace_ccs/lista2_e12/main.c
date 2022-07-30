87ce17c6f5ffe0e1b4e37f730e5cf3b656384a46
leonamcsg <leonamcsg@hotmail.com>
2022-07-29 21:48:09 -0300

    Piscando Led1 em aproximadamente 50Hz ultilizando interrupcoes do TimerA

--- workspace_ccs/lista2_e12/main.c
+++ workspace_ccs/lista2_e12/main.c
@@ -4,18 +4,17 @@
 /**
  * Leonam C. S. Gomes, 2022
  *
- * Piscando Led1 em aproximadamente 1Hz e Led2 em 2hz ultilizando
- * interrupcoes do TimerA.
+ * Piscando Led1 em aproximadamente 50Hz ultilizando interrupcoes do TimerA.
  */
 
 //
 //-------     -------     -------
 //|     |     |     |     |         Periodo do ciclo, Timer a partir de SMCLOCK
-//      -------     -------         1Mhrz/8 = 125khz, CCR0 = 62500, T0 = 500ms
+//      -------     -------         1Mhrz, CCR0 = 20000, T0 = 20ms,
 //
 //----  ----  ----  ----  ----  -
-//|  |  |  |  |  |  |  |  |  |  |   50% do ciclo de trabalho, 2Hz, modo alternado,
-//   ----  ----  ----  ----  ----   T1 = 250ms.
+//|  |  |  |  |  |  |  |  |  |  |   50% do ciclo de trabalho, 50Hz, modo alternado,
+//   ----  ----  ----  ----  ----   T1 = 10ms.
 //
 
 void IOconfig(void);
@@ -27,13 +26,12 @@ int main(void) {
 
     IOconfig();                         // Configura pinos de entrada e saida
 
-    initTimerA0();                      // Configura TimerA, a 1Mhz/8 = 125Khz
+    initTimerA0();                      // Configura TimerA, a 1Mhz
 
     __enable_interrupt();               // Habilita interrupcoes
 
-    TA0CCR0 = 62500 - 1;                // TimerA com 62500Ticks/125Khz = 500ms
-    TA0CCR1 = 62500 - 1;                // TimerA com 62500Ticks/125Khz = 500ms
-    TA0CCR2 = 31250 - 1;                // TimerA com 31250Ticks/125Khz = 250ms
+    TA0CCR0 = 20000 - 1;                // TimerA com 20000Ticks/1Mhz = 20ms
+    TA0CCR1 = 10000 - 1;                // TimerA com 10000Ticks/1Khz = 10ms
 
     __bis_SR_register(LPM0_bits);       // Ativa modo de baixo consumo
     __no_operation();                   // Aguarda interrupcao
@@ -42,22 +40,17 @@ int main(void) {
 }
 
 void IOconfig(void) {
-    P4OUT &= ~(BIT7);                   // inicia led desligado (ativo-alto)
-    P4DIR |= (BIT7);                    // pino P4.7 como saida
-
     P1OUT &= ~(BIT0);                   // inicia led desligado (ativo-alto)
     P1DIR |= (BIT0);                    // pino P1.0 como saida
 }
 
 void initTimerA0(void) {
     TA0CCTL0 |= CCIE;
-    TA0CCTL1 |= OUTMOD_4 | CCIE;        // Habilita interrupcao para CCR1, modo "toggle"
-    TA0CCTL2 |= OUTMOD_4 | CCIE;        // Habilita interrupcao para CCR2, modo "toggle"
-    TA0CTL = MC_1|ID_3|TASSEL_2|TACLR;  // Configura e inicia o Timer A
+    TA0CCTL1 |= CCIE;        // Habilita interrupcao para CCR1, modo "toggle"
+    TA0CTL = MC_1|ID_0|TASSEL_2|TACLR;  // Configura e inicia o Timer A
 
     TA0CCTL0 &= ~CCIFG;                 // Limpa flags de interrupcao
     TA0CCTL1 &= ~CCIFG;
-    TA0CCTL2 &= ~CCIFG;
 }
 
 //TimerA Rotina do Servico de Interrupcao
@@ -65,6 +58,7 @@ void initTimerA0(void) {
 #pragma vector = TIMER0_A0_VECTOR       // Tratamento para interrupcao CCR0IFG
 __interrupt void TIMERA0_CCR0_ISR(void) {
     TA0CCTL0 &= ~CCIFG;                 // Limpa flag de overflow
+    P1OUT ^= BIT0;                      // Altera estado led1
 }
 
 #pragma vector = TIMER0_A1_VECTOR       // Tratamento para interrupcao TA0IVIFG
@@ -72,18 +66,13 @@ __interrupt void TIMERA1_TA0IV_ISR(void) {
     switch (TA0IV) {                    // Verificando qual CCR ativou interrupcao
         case 0:
             break;
-        case TA0IV_TA0CCR1:             // Se CCR1 ativou a interrupcao
+        case TA0IV_TA0CCR1:             // Se CCR1 ativou a interrupcao,
             P1OUT ^= BIT0;              // Altera estado led1
-            TA0CCTL1 &= ~CCIFG;
-        case TA0IV_TA0CCR2:             // Se CCR2 ativou a interrupcao
-            P4OUT ^= BIT7;              // Altera estado Led2 novamente, em 50% DC
-            TA0CCTL2 &= ~CCIFG;         // gerando dobro da frequencia do Led1
             break;
         case TA0IV_TA0IFG:              // Possui tratamento especial
             break;
         default:
             break;
     }
-
 }
 
