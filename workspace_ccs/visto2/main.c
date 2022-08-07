65fc495e4e54b78d3dde2ee103be114aa22b2736
leonamcsg <leonamcsg@hotmail.com>
2022-08-06 23:32:10 -0300

    Trocando pinos

--- workspace_ccs/visto2/main.c
+++ workspace_ccs/visto2/main.c
@@ -1,11 +1,15 @@
-/* Leonam Christian Silva Gomes, 190043318
- */
+// Autor: Leonam Christian Silva Gomes, Matr.: 190043318
+
 #include <msp430.h>
 #include <stdint.h>
 #include <intrinsics.h>
 /**
  * Sensor ultrassonico HC-SR04 com mostrador analogico utilizando
  * servo motor SG90 Tower Pro e led1 e led2.
+ *
+ * P2.5 - Saida PWM mostrador analogico
+ * P1.5 - Sinal "trigger" sensor ultrassonico
+ * P2.0 - Sinal "echo" sensor ultrassonico
  */
 static const uint16_t lowlimit = 500 - 1; //  0 graus
 static volatile uint16_t newdistance;
@@ -15,27 +19,29 @@ void IOconfig(void);
 
 void initTimers(void);
 
+
+
 int main(void) {
     WDTCTL = WDTPW | WDTHOLD;               // Para WDT
 
     static const uint16_t uplimit = 2500 - 1; //  180 gaus
-    static volatile uint16_t newTA2CCR2;
+    static volatile uint16_t newTA1CCR1;
 
     IOconfig();                             // Configura pinos de entrada e saida
     initTimers();                           // Inicia temporizadores
     __enable_interrupt();                   // Habilita interrupcoes, GIE bit em SR;
     for(;;) {
 
-        while(!PRONTO);                     // Aguarda sinal de TA0CCR2
+        while(!PRONTO);                     // Aguarda sinal de TA1CCR1
         PRONTO = 0;
 
-        newTA2CCR2 = newdistance / 1.45 + lowlimit;
+        newTA1CCR1 = newdistance / 1.45 + lowlimit;
 
         // Atualiza PWM P2.5 com nova distancia
-        if(newTA2CCR2 >= uplimit) {          // Caso CCR1 maior que limite superior
-            TA2CCR2 = uplimit;               // Limitando maior valor de CCR2
+        if(newTA1CCR1 >= uplimit) {          // Caso TA1CCR1 maior que limite superior
+            TA2CCR2 = uplimit;               // Limitando maior valor de TA2CCR2
         } else {
-            TA2CCR2 = newTA2CCR2;            // Atualiza TACCR2 com novo tempo
+            TA2CCR2 = newTA1CCR1;            // Atualiza TACCR2 com novo tempo
         }
 
         // LEDs
@@ -64,55 +70,61 @@ void IOconfig(void) {
     P2DIR |= BIT5;              // P2.5 como saida
     P2SEL |= BIT5;              // Seleciona P2.5 como saida PWM
 
-    P1DIR |= BIT2;              // Sinal trigger(saida)
-    P1DIR &= ~BIT3;             // Sinal echo(captura)
-    P1SEL |= BIT2 | BIT3;       // P1.2 como TA0CCR1 e P1.3 como TA0CCR2
+    P1DIR |= BIT5;              // Sinal trigger(saida)
+    P2DIR &= ~BIT0;             // Sinal echo(captura)
+    P1SEL |= BIT5;              // P1.5 como TA0CCR4
+    P2SEL |= BIT0;              // P2.0 como TA1CCR1
 }
 
 void initTimers(void) {
 
-    // TimerA2 PWM P2.5
+    // TimerA2 Servo Motor, P2.5(PWM)
     TA2CCR0 = 20000 - 1;                      // PWM periodo
     TA2CCTL2 = OUTMOD_7;                      // CCR2 reset/set
     TA2CCR2 = lowlimit;                       // CCR2 PWM duty cycle
     TA2CTL = TASSEL__SMCLK | MC__UP | TACLR;  // SMCLK, up mode, limpa TAR
 
-    // TimerA0 Sensor Ultrassonico, Trigger -> P1.2(PWM), Echo -> P1.2(CAP)
+    // TimerA0 Sensor Ultrassonico, Trigger -> P1.5(PWM)
+    TA0CCR0 = 12000 - 1;                      // Periodo total, trigger-echo = 12ms
+    TA0CCR4 = 10  - 1;                        // Pulso trigger dura 10us
+    TA0CCTL4 |= OUTMOD_7;                     // Modo "set/reset"
+
     TA0CTL |= TASSEL__SMCLK |                 // SMCLK = 1Mhz
               MC__UP        |                 // Modo "up"
               TACLR;                          // Limpa TA0R
 
-    TA0CCR0 = 12000 - 1;                      // Periodo total, trigger ate tempo maximo 12ms
-    TA0CCR1 = 10 - 1;                         // Pulso trigger dura 10us
-
-    TA0CCTL1 |= OUTMOD_7;                     // Modo "set/reset"
-
-    TA0CCTL2 |= CAP  |                        // Modo captura
+    // TimerA1 Sensor Ultrassonico, Echo -> P2.0(CAP)
+    TA1CCR0 = 12000 - 1;                      // Periodo total 12ms
+    TA1CCTL1 |= CAP  |                        // Modo captura
                 CM_3 |                        // Modo de subida e descida do CLK
-                CCIE;                         // Habilita interrupcao TA0CCR2
+                CCIE;                         // Habilita interrupcao TA1CCR1
+
+    TA1CTL |= TASSEL__SMCLK |                 // SMCLK = 1Mhz
+              MC__UP        |                 // Modo "up"
+              TACLR;                          // Limpa TA0R
 }
 
-// Servico de inrrupcao de rotina TimerA0
-#pragma vector = TIMER0_A1_VECTOR
-__interrupt void timerA0_TA0IV_ISR(void) {
+// Servico de inrrupcao de rotina TimerA1
+#pragma vector = TIMER1_A1_VECTOR
+__interrupt void timerA1_TA1IV_ISR(void) {
     static volatile uint16_t count = 0;
     static volatile uint16_t lastcaptured = 0;
 
-    switch (TA0IV) {                          // Leitura limpa flag de interrupcao
+    switch (TA1IV) {                          // Leitura limpa flag de interrupcao
         case 0:
             break;
 
-        case TA0IV_TA0CCR2:                   // Se CCR2 ativou interrupcao
+        case TA1IV_TA1CCR1:                   // Se CCR1 ativou interrupcao
             if(count != 0) {                  // Conta subida e descida
                 PRONTO = 1;                   // Sinaliza processo para main()
                 count = 0;
             } else {
                 count = 1;                    // Contagem
             }
-            newdistance = TA0CCR2 - lastcaptured; // 0 a 12ms
-            lastcaptured = TA0CCR2;
+            newdistance = TA1CCR1 - lastcaptured; // 0 a 12ms
+            lastcaptured = TA1CCR1;
 
-            TA0CCTL2 &= ~CCIFG;                // Limpa flag de interrupcao de CCR2
+            TA1CCTL1 &= ~CCIFG;               // Limpa flag de interrupcao de CCR1
             break;
 
         default:
